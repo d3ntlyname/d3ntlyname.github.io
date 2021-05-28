d80b300ef04385c3ada6860c045aba387912f68c
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-05-28 23:14:55 +0300

    Create pidormazer.py

---
+++ modules/pidormazer.py
@@ -0,0 +1,14 @@
+from .. import loader, utils 
+import re, random 
+@loader.tds 
+class PidorMazerMod(loader.Module): 
+    strings = {"name": "Пидормайзер"} 
+    prefix = "<b>[Пидормайзер]</b>\n" 
+    @loader.owner 
+
+    async def pidrchcmd(self, m): 
+        ".pidrch покажу пидора в чате!:)" 
+        if not m.chat: await m.edit.self.prefix+"😐 <b>| Это не чат, пидрило</b>"); return 
+        users = await m.client.get_participants(m.chat) 
+        user = random.choice(users) 
+        await m.edit(self.prefix+f"🍁 <b>| Режим:</b> ПидорМейзер, покажу пидора чата\n<b>Пидор чата:</b> <a href=\"tg://user?id={user.id}\">{user.first_name}</a> | <code>{user.id}</code>")
