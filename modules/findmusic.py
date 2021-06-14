789f6b0ba403094bb290bca876027fc3b658173f
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-06-14 17:50:13 +0300

    Update findmusic.py

--- modules/findmusic.py
+++ modules/findmusic.py
@@ -5,46 +5,45 @@ from .. import loader, utils
 @loader.tds 
 class SearchMusicMod(loader.Module): 
     """
-Модуль FindMusic - поиск музыки
-Работает через бота lybot, vkm_bot и muzyka_tiktok_bot 
+Модуль FindMusic — поиск музыки
     """ 
     strings = {"name": "FindMusic"} 
  
     async def ytmcmd(self, message): 
-        """Используй: .ytm <название>, чтобы найти музыку по названию в YouTube""" 
+        """.ytm <название>, чтобы найти музыку по названию в YouTube""" 
         args = utils.get_args_raw(message) 
         reply = await message.get_reply_message() 
         if not args: 
-            return await message.edit("<b>[FindMusic] Нету аргументов...</b>")  
+            return await message.edit("<b>[FindMusic] Нету аргументов</b>")  
         try: 
             await message.edit("<b>[FindMusic] Загрузка...</b>") 
             music = await message.client.inline_query('lybot', args) 
             await message.delete() 
             await message.client.send_file(message.to_id, music[0].result.document, reply_to=reply.id if reply else None) 
-        except: return await message.client.send_message(message.chat_id, f"<b>[FindMusic] Музыка с названием <code>{args}</code> не найдена...</b>")
+        except: return await message.client.send_message(message.chat_id, f"<b>[FindMusic] Музыка с названием <code>{args}</code> не найдена</b>")
 
     async def vkmcmd(self, message): 
-        """Используй: .vkm <название>, чтобы найти музыку по названию в ВКонтакте""" 
+        """.vkm <название>, чтобы найти музыку по названию в ВКонтакте""" 
         args = utils.get_args_raw(message) 
         reply = await message.get_reply_message() 
         if not args: 
-            return await message.edit("<b>[FindMusic] Нету аргументов...</b>")  
+            return await message.edit("<b>[FindMusic] Нету аргументов</b>")  
         try: 
             await message.edit("<b>[FindMusic] Загрузка...</b>") 
             music = await message.client.inline_query('vkm_bot', args) 
             await message.delete() 
             await message.client.send_file(message.to_id, music[0].result.document, reply_to=reply.id if reply else None) 
-        except: return await message.client.send_message(message.chat_id, f"<b>[FindMusic] Музыка с названием <code>{args}</code> не найдена...</b>")
+        except: return await message.client.send_message(message.chat_id, f"<b>[FindMusic] Музыка с названием <code>{args}</code> не найдена</b>")
 
     async def ttmcmd(self, message): 
-        """Используй: .ttm <название>, чтобы найти музыку по названию в TikTok""" 
+        """.ttm <название>, чтобы найти музыку по названию в TikTok""" 
         args = utils.get_args_raw(message) 
         reply = await message.get_reply_message() 
         if not args: 
-            return await message.edit("<b>[FindMusic] Нету аргументов...</b>")  
+            return await message.edit("<b>[FindMusic] Нету аргументов</b>")  
         try: 
             await message.edit("<b>[FindMusic] Загрузка...</b>") 
             music = await message.client.inline_query('muzyka_tiktok_bot', args) 
             await message.delete() 
             await message.client.send_file(message.to_id, music[0].result.document, reply_to=reply.id if reply else None) 
-        except: return await message.client.send_message(message.chat_id, f"<b>[FindMusic] Музыка с названием <code>{args}</code> не найдена...</b>")
+        except: return await message.client.send_message(message.chat_id, f"<b>[FindMusic] Музыка с названием <code>{args}</code> не найдена</b>")
