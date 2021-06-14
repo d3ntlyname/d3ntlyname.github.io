1661e10fed451cf2f0a8fe47ce6efaf4234e52b3
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-06-14 19:26:08 +0300

    Update findmusic.py

--- modules/findmusic.py
+++ modules/findmusic.py
@@ -20,7 +20,7 @@ class SearchMusicMod(loader.Module):
             music = await message.client.inline_query('lybot', args) 
             await message.delete() 
             await message.client.send_file(message.to_id, music[0].result.document, reply_to=reply.id if reply else None) 
-        except: return await message.client.send_message(message.chat_id, f"<b>[FindMusic] Музыка с названием <code>{args}</code> не найдена</b>"); await message.delete()
+        except: return await message.client.send_message(message.chat_id, f"<b>[FindMusic] Музыка с названием <code>{args}</code> не найдена</b>")
 
     async def vkmcmd(self, message): 
         """.vkm <название>, чтобы найти музыку по названию в ВКонтакте""" 
@@ -33,7 +33,7 @@ class SearchMusicMod(loader.Module):
             music = await message.client.inline_query('vkm_bot', args) 
             await message.delete() 
             await message.client.send_file(message.to_id, music[0].result.document, reply_to=reply.id if reply else None) 
-        except: return await message.client.send_message(message.chat_id, f"<b>[FindMusic] Музыка с названием <code>{args}</code> не найдена</b>"); await message.delete()
+        except: return await message.client.send_message(message.chat_id, f"<b>[FindMusic] Музыка с названием <code>{args}</code> не найдена</b>")
 
     async def ttmcmd(self, message): 
         """.ttm <название>, чтобы найти музыку по названию в TikTok""" 
@@ -46,4 +46,4 @@ class SearchMusicMod(loader.Module):
             music = await message.client.inline_query('muzyka_tiktok_bot', args) 
             await message.delete() 
             await message.client.send_file(message.to_id, music[0].result.document, reply_to=reply.id if reply else None) 
-        except: return await message.client.send_message(message.chat_id, f"<b>[FindMusic] Музыка с названием <code>{args}</code> не найдена</b>"); await message.delete()
+        except: return await message.client.send_message(message.chat_id, f"<b>[FindMusic] Музыка с названием <code>{args}</code> не найдена</b>")
