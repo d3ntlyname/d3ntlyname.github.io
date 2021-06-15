d2af6d405ecc10439501a56d362fd13af94475b3
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-06-15 18:53:24 +0300

    Update unblock.py

--- modules/unblock.py
+++ modules/unblock.py
@@ -1,5 +1,8 @@
 from .. import loader, utils 
 from telethon import functions 
+from telethon.tl.functions.contacts import BlockRequest 
+from telethon.tl.functions.messages import ReportSpamRequest 
+
  
 @loader.tds 
 class UnBlockNaxyiMod(loader.Module): 
@@ -7,6 +10,7 @@ class UnBlockNaxyiMod(loader.Module):
     strings={"name": "UnBlock"} 
  
     async def unblockcmd(self, message): 
+"""Удалить юзера с списка"""
         args = utils.get_args_raw(message) 
         reply = await message.get_reply_message() 
         if not reply: 
@@ -21,3 +25,18 @@ class UnBlockNaxyiMod(loader.Module):
                                                                       add_phone_privacy_exception=False)) 
             await message.edit(f"<b><a href='tg://user?id={user.id}'>{user.first_name}</a> удалён из списка AutoBlackList</b>") 
         except: return await message.edit("<b>Даун, что-то пошло по пизде</b>")
+
+    async def blockcmd(self, message): 
+  """Добавить юзера в список"""
+        args = utils.get_args_raw(message) 
+        reply = await message.get_reply_message() 
+        if not reply: 
+            return await message.edit("<b>Даун, а где реплай?</b>") 
+        else: 
+            user = await message.client.get_entity(reply.sender_id) 
+        try: 
+            await message.client(BlockRequest(user.id)) 
+            await message.client(ReportSpamRequest(user.id)) 
+            await message.edit(f"<b><a href='tg://user?id={user.id}'>{user.first_name}</a> добавлен в список AutoBlackList</b>") 
+        except: return await message.edit("<b>Даун, что-то пошло по пизде</b>")
+
