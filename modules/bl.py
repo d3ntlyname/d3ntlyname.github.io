695116e9a608e247fd848fff4cc6b97e83d37c6d
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-06-15 18:23:55 +0300

    Update bl.py

--- modules/bl.py
+++ modules/bl.py
@@ -1,15 +1,17 @@
-# @Sekai_Yoneya
+# by @dently
 
 from .. import loader, utils 
 from telethon.tl.functions.contacts import BlockRequest 
 from telethon.tl.functions.messages import ReportSpamRequest 
+from .. import loader, utils 
+from telethon import functions 
  
  
 def register(cb): 
     cb(AutoBlackListMod()) 
  
 class AutoBlackListMod(loader.Module): 
-    """Кидает всех неконтактов в ЧС.""" 
+    """Кидает всех неконтактов в чёрный список""" 
     strings = {'name': 'AutoBlackList'} 
  
     async def client_ready(self, client, db): 
@@ -22,14 +24,14 @@ class AutoBlackListMod(loader.Module):
         if args: 
             self.db.set("AutoBlackList", "status", True) 
             self.db.set("AutoBlackList", "message", str(args)) 
-            return await message.edit("<b>[AutoBlackList]</b> Активирован!") 
+            return await message.edit("<b>[AutoBlackList] Активирован!</b>") 
      
         if autobl == False: 
             self.db.set("AutoBlackList", "status", True) 
             self.db.set("AutoBlackList", "message", "") 
-            return await message.edit("<b>[AutoBlackList Mode]</b> Активирован!") 
+            return await message.edit("<b>[AutoBlackList] Активирован!</b>") 
         self.db.set("AutoBlackList", "status", False) 
-        return await message.edit("<b>[AutoBlackList Mode]</b> Деактивирован!") 
+        return await message.edit("<b>[AutoBlackList] Деактивирован!</b>") 
  
  
     async def autoblstatuscmd(self, message): 
@@ -44,11 +46,41 @@ class AutoBlackListMod(loader.Module):
         autodel = self.db.get("AutoBlackList", "delchat", False) 
         if autodel == False: 
             self.db.set("AutoBlackList", "delchat", True) 
-            return await message.edit("<b>[AutoBlackList Mode - DelChat]</b> Активирован!") 
+            return await message.edit("<b>[AutoBlackList • DelChat] Активирован!</b>") 
         self.db.set("AutoBlackList", "delchat", False) 
-        return await message.edit("<b>[AutoBlackList Mode - DelChat]</b> Деактивирован!") 
+        return await message.edit("<b>[AutoBlackList • DelChat] Деактивирован!</b>") 
  
- 
+    async def unblockcmd(self, message): 
+  """Удалить юзера с списка"""
+        args = utils.get_args_raw(message) 
+        reply = await message.get_reply_message() 
+        if not reply: 
+            return await message.edit("<b>Даун, а где реплай?</b>") 
+        else: 
+            user = await message.client.get_entity(reply.sender_id) 
+        try: 
+            await message.client(functions.contacts.AddContactRequest(id=user.id,  
+                                                                      first_name=user.first_name, 
+                                                                      last_name=' ', 
+                                                                      phone='мобила', 
+                                                                      add_phone_privacy_exception=False)) 
+            await message.edit(f"<b><a href='tg://user?id={user.id}'>{user.first_name}</a> удален из списка AutoBlackList</b>") 
+        except: return await message.edit("<b>Даун, что-то пошло по пизде</b>")
+
+    async def blockcmd(self, message): 
+  """Добавить юзера в список"""
+        args = utils.get_args_raw(message) 
+        reply = await message.get_reply_message() 
+        if not reply: 
+            return await message.edit("<b>Даун, а где реплай?</b>") 
+        else: 
+            user = await message.client.get_entity(reply.sender_id) 
+        try: 
+            await message.client(BlockRequest(user.id)) 
+            await message.client(ReportSpamRequest(user.id)) 
+            await message.edit(f"<b><a href='tg://user?id={user.id}'>{user.first_name}</a> добавлен в список AutoBlackList</b>") 
+        except: return await message.edit("<b>Даун, что-то пошло по пизде</b>")
+
     async def watcher(self, message): 
         """Вау, это watcher, я что-то смог из него сделать. Поздравьте меня)""" 
         try: 
