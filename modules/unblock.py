260a645c81db087b5d2e3aadb5db58606b402c8b
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-06-15 15:17:57 +0300

    Create unblock.py

---
+++ modules/unblock.py
@@ -0,0 +1,25 @@
+# by @dently
+
+from .. import loader, utils 
+from telethon import functions 
+ 
+@loader.tds 
+class UnBlockNaxyiMod(loader.Module): 
+    """Удалить юзера с списка авточс""" 
+    strings={"name": "UnBlock"} 
+ 
+    async def unblockcmd(self, message): 
+        args = utils.get_args_raw(message) 
+        reply = await message.get_reply_message() 
+        if not reply: 
+            return await message.edit("<b>Даун, а где реплай?</b>") 
+        else: 
+            user = await message.client.get_entity(reply.sender_id) 
+        try: 
+            await message.client(functions.contacts.AddContactRequest(id=user.id,  
+                                                                      first_name=user.first_name, 
+                                                                      last_name=' ', 
+                                                                      phone='мобила', 
+                                                                      add_phone_privacy_exception=False)) 
+            await message.edit(f"<b>{user.first_name} удален из списка авточс</b>") 
+        except: return await message.edit("<b>Даун, что-то пошло по пизде</b>")
