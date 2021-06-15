8d641747c9c6812906648a1d21b029ee039bbfc1
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-06-15 19:01:06 +0300

    Update unblock.py

--- modules/unblock.py
+++ modules/unblock.py
@@ -1,8 +1,7 @@
+# by @dently
+
 from .. import loader, utils 
 from telethon import functions 
-from telethon.tl.functions.contacts import BlockRequest 
-from telethon.tl.functions.messages import ReportSpamRequest 
-
  
 @loader.tds 
 class UnBlockNaxyiMod(loader.Module): 
@@ -10,7 +9,6 @@ class UnBlockNaxyiMod(loader.Module):
     strings={"name": "UnBlock"} 
  
     async def unblockcmd(self, message): 
-  """Удалить юзера с списка"""
         args = utils.get_args_raw(message) 
         reply = await message.get_reply_message() 
         if not reply: 
@@ -23,20 +21,5 @@ class UnBlockNaxyiMod(loader.Module):
                                                                       last_name=' ', 
                                                                       phone='мобила', 
                                                                       add_phone_privacy_exception=False)) 
-            await message.edit(f"<b><a href='tg://user?id={user.id}'>{user.first_name}</a> удалён из списка AutoBlackList</b>") 
+            await message.edit(f"<b><a href='tg://user?id={user.id}'>{user.first_name}</a> удален из списка AutoBlackList</b>") 
         except: return await message.edit("<b>Даун, что-то пошло по пизде</b>")
-
-    async def blockcmd(self, message): 
-  """Добавить юзера в список"""
-        args = utils.get_args_raw(message) 
-        reply = await message.get_reply_message() 
-        if not reply: 
-            return await message.edit("<b>Даун, а где реплай?</b>") 
-        else: 
-            user = await message.client.get_entity(reply.sender_id) 
-        try: 
-            await message.client(BlockRequest(user.id)) 
-            await message.client(ReportSpamRequest(user.id)) 
-            await message.edit(f"<b><a href='tg://user?id={user.id}'>{user.first_name}</a> добавлен в список AutoBlackList</b>") 
-        except: return await message.edit("<b>Даун, что-то пошло по пизде</b>")
-
