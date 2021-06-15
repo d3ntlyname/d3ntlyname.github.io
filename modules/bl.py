17d13ab53fc20dec21faf7e3f0554c329e3b8117
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-06-15 18:27:45 +0300

    Update bl.py

--- modules/bl.py
+++ modules/bl.py
@@ -3,7 +3,6 @@
 from .. import loader, utils 
 from telethon.tl.functions.contacts import BlockRequest 
 from telethon.tl.functions.messages import ReportSpamRequest 
-from .. import loader, utils 
 from telethon import functions 
  
  
@@ -40,7 +39,6 @@ class AutoBlackListMod(loader.Module):
                            f"<b>Кидать в ЧС</b> - {self.db.get('AutoBlackList', 'status')}\n" 
                            f"<b>Удалять чаты</b> - {self.db.get('AutoBlackList', 'delchat')}") 
  
- 
     async def autodelchatcmd(self, message): 
         """Автоматически удаляет диалог после того, как кинет в ЧС""" 
         autodel = self.db.get("AutoBlackList", "delchat", False) 
@@ -50,36 +48,6 @@ class AutoBlackListMod(loader.Module):
         self.db.set("AutoBlackList", "delchat", False) 
         return await message.edit("<b>[AutoBlackList • DelChat] Деактивирован!</b>") 
  
-    async def unblockcmd(self, message): 
-  """Удалить юзера с списка"""
-        args = utils.get_args_raw(message) 
-        reply = await message.get_reply_message() 
-        if not reply: 
-            return await message.edit("<b>Даун, а где реплай?</b>") 
-        else: 
-            user = await message.client.get_entity(reply.sender_id) 
-        try: 
-            await message.client(functions.contacts.AddContactRequest(id=user.id,  
-                                                                      first_name=user.first_name, 
-                                                                      last_name=' ', 
-                                                                      phone='мобила', 
-                                                                      add_phone_privacy_exception=False)) 
-            await message.edit(f"<b><a href='tg://user?id={user.id}'>{user.first_name}</a> удален из списка AutoBlackList</b>") 
-        except: return await message.edit("<b>Даун, что-то пошло по пизде</b>")
-
-    async def blockcmd(self, message): 
-  """Добавить юзера в список"""
-        args = utils.get_args_raw(message) 
-        reply = await message.get_reply_message() 
-        if not reply: 
-            return await message.edit("<b>Даун, а где реплай?</b>") 
-        else: 
-            user = await message.client.get_entity(reply.sender_id) 
-        try: 
-            await message.client(BlockRequest(user.id)) 
-            await message.client(ReportSpamRequest(user.id)) 
-            await message.edit(f"<b><a href='tg://user?id={user.id}'>{user.first_name}</a> добавлен в список AutoBlackList</b>") 
-        except: return await message.edit("<b>Даун, что-то пошло по пизде</b>")
 
     async def watcher(self, message): 
         """Вау, это watcher, я что-то смог из него сделать. Поздравьте меня)""" 
