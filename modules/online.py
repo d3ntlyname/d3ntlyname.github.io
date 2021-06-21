655b697d1b4f2a4ad3a56bb07bce4e4e142aef59
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-06-21 23:18:32 +0300

    Create online.py

---
+++ modules/online.py
@@ -0,0 +1,31 @@
+# by @dently
+
+from .. import loader 
+from asyncio import sleep 
+ 
+@loader.tds 
+class EternalOnlineMod(loader.Module): 
+    """Вечный онлайн.""" 
+    strings = {'name': 'Online'} 
+ 
+    async def client_ready(self, client, db): 
+        self.db = db 
+ 
+    async def onlinecmd(self, message): 
+        """Включить вечный онлайн""" 
+        if not self.db.get("Eternal Online", "status"): 
+            self.db.set("Eternal Online", "status", True) 
+            await message.edit("Вечный онлайн включен") 
+            while self.db.get("Eternal Online", "status"): 
+                msg = await message.client.send_message("me", "Telegram best messenger! 🤩")
+                await msg.delete()
+                await sleep(70) 
+ 
+        else: 
+            self.db.set("Eternal Online", "status", False) 
+            await message.edit("Вечный онлайн выключен")
+
+    async def watcher(self, message): 
+        """Вау, это watcher, я что-то смог из него сделать. Поздравьте меня)""" 
+        if message.is_private: 
+            await message.client.send_read_acknowledge(message.chat_id, clear_mentions=True)
