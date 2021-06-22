5330c504aa4e3d11c680b1895dcabfd9e75fa179
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-06-22 21:04:16 +0300

    Update voice.py

--- modules/voice.py
+++ modules/voice.py
@@ -29,6 +29,7 @@ class VoiceEMod(loader.Module):
                         await message.client.send_message(chat, reply) 
                         response = await response 
                         r = "<code>" + response.text + "</code>"
+                        r = r.replace("При поддержке Бородач Клуба", "")
                     except YouBlockedUserError: 
                         await message.reply("<b>Сними <a href='tg://user?id=259276793'>Voicy</a> с ЧС</b>") 
                         return 
@@ -44,6 +45,7 @@ class VoiceEMod(loader.Module):
                         await message.client.send_message(chat, reply) 
                         response = await response 
                         r = "<code>" + response.text + "</code>"
+                        r = r.replace("При поддержке Бородач Клуба", "")
                     except YouBlockedUserError: 
                         await message.reply("<b>Сними <a href='tg://user?id=259276793'>Voicy</a> с ЧС</b>") 
                         return 
