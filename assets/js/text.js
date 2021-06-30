b23a9457a3f043327005f9836cb1edce0d39d983
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-06-30 16:59:49 +0300

    Update text.js

--- assets/js/text.js
+++ assets/js/text.js
@@ -1,6 +1,6 @@
 $(function () {
     var title = document.title,
-        animSeq = [""],
+        animSeq = ["ᅠ"],
         animIndex = 0,
         titleIndex = 0;
     function doInverseSpinZeroPitch() {
