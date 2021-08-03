e145228e91d4e350f3a20add47c4f20ce02876e1
d3ntlyname <84956808+d3ntlyname@users.noreply.github.com>
2021-08-03 21:06:58 +0300

    Update text.js

--- assets/js/text.js
+++ assets/js/text.js
@@ -21,7 +21,7 @@ $(function () {
 });
 $(function () {
     $(".typed").typed({
-        strings: ["python developer", "(nedo)coder", "c# beginner", "music lover...", "hmmm...", "rm -rf \/\*", "bruh",
+        strings: ["python developer", "(nedo)coder", "c# beginner", "music lover...", "hmmm...", "rm -rf \/\*", "bruh"],
         typeSpeed: 35,
         backSpeed: 0,
         cursorChar: ['❚'],
